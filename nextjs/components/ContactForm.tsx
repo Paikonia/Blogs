a666b91056e0ccfb22a1e4dfcae2dd00190ec534
Aiko Patrick <patrick.aikonia@gmail.com>
2024-01-12 13:38:43 +0300

    Almost complete build for nextjs

--- nextjs/components/ContactForm.tsx
+++ nextjs/components/ContactForm.tsx
@@ -3,17 +3,20 @@ import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
 
+
+
 const ContactForm = () => {
+  
   return (
     <div>
-      <form name="Contact Form" method="POST" action="/contact-thank-you">
+      <form name="Contact Form" method="POST">
         <Label htmlFor="Name">Name</Label>
         <Input type="text" name="Name" className="my-4" placeholder="John Doe" />
         <Label htmlFor="Email">Email</Label>
         <Input type="email" name="Email" className="my-4" placeholder="hello@example.com" />
         <Label htmlFor="Message">Message</Label>
         <Textarea name="Message" placeholder="Your message here..." />
-        <Button disabled>Submit</Button>
+        <Button disabled type="submit">Submit</Button>
       </form>
     </div>
   );
